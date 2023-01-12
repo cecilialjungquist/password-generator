867081afb544ebeaa919c6959a9d0bb841556d24
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-01-12 14:54:35 +0100

    variables

--- app.js
+++ app.js
@@ -12,11 +12,11 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let generateBtn = document.getElementById('generate-btn');
-let passwordDisplList = document.querySelectorAll('.p');
-let symbolsCheckbox = document.getElementById('symbols-checkbox');
+const generateBtn = document.getElementById('generate-btn');
+const passwordDisplList = document.querySelectorAll('.p');
+const symbolsCheckbox = document.getElementById('symbols-checkbox');
 let isChecked = true;
-let helpText = document.querySelector('.help-text');
+const helpText = document.querySelector('.help-text');
 
 
 
