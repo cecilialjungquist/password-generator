9cf6cb676923c7e4357fef1221f99b6cd6eb8945
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-01-13 10:58:52 +0100

    minor updates

--- app.js
+++ app.js
@@ -9,8 +9,8 @@
 // * Add "copy on click" - check
 // * Toggle symbols (and numbers) on/off - check
 
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
 
 const generateBtn = document.getElementById('generate-btn');
 let passwordDisplList = document.querySelectorAll('.p');
@@ -20,7 +20,7 @@ const helpText = document.querySelector('.help-text');
 
 
 
-symbolsCheckbox.addEventListener('change', function() {
+symbolsCheckbox.addEventListener('change', function () {
     if (symbolsCheckbox.checked === true) {
         isChecked = true;
     } else {
@@ -30,7 +30,7 @@ symbolsCheckbox.addEventListener('change', function() {
 
 
 function getRandomNumber(arrayLength) {
-    let randomNumber = Math.floor( Math.random() * arrayLength );
+    let randomNumber = Math.floor(Math.random() * arrayLength);
     return randomNumber;
 };
 
@@ -40,9 +40,9 @@ function generatePassword(length) {
 
     if (!isChecked) {
         // Om isChecked är false, ändra arrayLength till första tecknet
-        arrayLength  = characters.findIndex(element => element === "~");
-    } 
-                    // Ändra till password length
+        arrayLength = characters.findIndex(element => element === "~");
+    }
+
     for (let i = 0; i < length; i++) {
         let randomIndex = getRandomNumber(arrayLength);
         newPassword += characters[randomIndex];
@@ -52,16 +52,15 @@ function generatePassword(length) {
 };
 
 // Lägger upp nytt password
-generateBtn.addEventListener('click', function() {
+generateBtn.addEventListener('click', function () {
 
     let passwordLength = document.getElementById('length-input').value;
-  
+
     if (passwordLength < 8 || passwordLength > 16) {
         helpText.style.display = 'block';
     } else {
         helpText.style.display = 'none';
         passwordDisplList.forEach(display => {
-
             display.textContent = generatePassword(passwordLength);
             display.style.padding = '.5rem';
         });
@@ -70,7 +69,7 @@ generateBtn.addEventListener('click', function() {
 
 // Kopierar password
 passwordDisplList.forEach(display => {
-    display.addEventListener('click', function() {
+    display.addEventListener('click', function () {
         navigator.clipboard.writeText(this.innerHTML);
         this.innerHTML = 'Copied!'
     });
